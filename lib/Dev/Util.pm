57cc1836f36f56f595d331987f2d06707f910c75
Matt Martini <matt.martini@gmail.com>
2025-11-20 18:31:15 -0500

    style(spelling): Fix typo, add to dictionary

--- lib/Dev/Util.pm
+++ lib/Dev/Util.pm
@@ -6,7 +6,7 @@ use warnings;
 use version;
 use Carp qw(carp);
 
-our $VERSION = version->declare("v2.19.8");
+our $VERSION = version->declare("v2.19.9");
 
 use Exporter   qw( );
 use List::Util qw( uniq );
@@ -55,27 +55,29 @@ Dev::Util - Utilities useful in the development of perl programs
 
 =head1 VERSION
 
-Version v2.19.8
+Version v2.19.9
 
 =head1 SYNOPSIS
 
 This module provides a standard set of tools to use for oft needed functionality.
 
 Consistent feature setup is achieved.
-Standard constants are defined. OS identification and external executables are accessible. 
-Quick backups can be made. File and directory attributes are discovered. Lockfiles
-are created.
+Standard constants are defined. OS identification and external executables are
+accessible. Quick backups can be made. File and directory attributes are discovered.
+Lock files are created.
 
 The sub-modules provide this and other utility functionality.
 
 =head1 SUB-MODULES
 
-The sub-modules provide the functionality described below.  For more details see C<<< perldoc <Sub-module_Name> >>>.
+The sub-modules provide the functionality described below.  For more details
+see C<<< perldoc <Sub-module_Name> >>>.
 
 
 =head2 Dev::Util
 
-C<Dev::Util> provides a loader for sub-modules where a leading C<::> denotes a package to load.
+C<Dev::Util> provides a loader for sub-modules where a leading C<::> denotes
+a package to load.
 
     use Dev::Util qw( ::File ::OS );
 
@@ -104,15 +106,15 @@ to all of the programs that use C<Dev::Util::Syntax>
 Use this in other modules:
 
     package My::Module::Example;
-    
+
     use Dev::Util::Syntax;
-    
+
     # Rest of Code...
 
 This is equivalent to:
 
     package My::Module::Example;
-    
+
     use feature :5.18;
     use utf8;
     use strict;
@@ -123,7 +125,7 @@ This is equivalent to:
     use Readonly;
     use Carp;
     use English qw( -no_match_vars );
-    
+
     # Rest of Code...
 
 B<Note: C<use Dev::Util::Syntax> automatically adds C<use strict> and C<use warnings> to the program.>
@@ -145,7 +147,7 @@ Defines named constants as Readonly, based on best practices.
 OS discovery and functions to execute and collect data from external programs.
 
     use Dev::Util::OS;
-    
+
     my $OS = get_os();
     my $hostname = get_hostname();
     my $system_is_linux = is_linux();
@@ -157,7 +159,7 @@ OS discovery and functions to execute and collect data from external programs.
 Provides functions to assist working with files and dirs, menus and prompts.
 
     use Dev::Util::File;
-    
+
     my $fexists     = file_exists('/path/to/somefile');
     my $canwritef   = file_writable('/path/to/somefile');
     my $isplainfile = file_is_plain('/path/to/somefile');
@@ -190,7 +192,7 @@ appended. Directories are backed up by tar and gz.
 Module to do Semaphore locking
 
     use Dev::Util::Sem;
-    
+
     my $sem = Sem->new('mylock.sem');
     ...
     $sem->unlock;
